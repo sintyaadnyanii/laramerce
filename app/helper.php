b95d05487e800bf1a7a046ecc9f52bb1158b9b4f
febryntara <paglutgede@gmail.com>
2022-12-27 15:34:30 +0800

    init helper

---
+++ app/helper.php
@@ -0,0 +1,32 @@
+<?php
+
+if (!function_exists('get_segment_url')) {
+    function get_segment_url()
+    {
+        $segment = [];
+        $current_url = explode('/', url()->current());
+        foreach ($current_url as $key => $value) {
+            if ($key > 2) {
+                $segment[] = $value;
+            }
+        }
+        return $segment;
+    }
+}
+
+if (!function_exists('ch_lang')) {
+    function ch_lang($idn, $en)
+    {
+        return session()->get('applocale') == 'id' ? $idn : $en;
+    }
+}
+
+if (!function_exists('ch_currency')) {
+    function ch_currency($idr, $usd = false)
+    {
+        if ($usd == false) {
+            return 'Idr ' . number_format($idr, 0, ".", ",");
+        }
+        return (session()->get('appcurrency') == 'idr' ? 'Idr ' : '$ ') . number_format(session()->get('appcurrency') == 'idr' ? $idr : $usd, 0, ".", ",");
+    }
+}
