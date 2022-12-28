c3e03412ed83e483a42df1e0de927a255f117015
febryntara <paglutgede@gmail.com>
2022-12-28 10:37:13 +0800

    fix tailwind watch

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -9,4 +9,33 @@ class Product extends Model
 {
     use HasFactory;
     protected $fillable = ['name', 'code', 'category_id', 'contition', 'price', 'weight', 'stock'];
+
+    public static function boot()
+    {
+        parent::boot();
+
+        self::creating(function ($model) {
+            // ... code here
+        });
+
+        self::created(function ($model) {
+            // ... code here
+        });
+
+        self::updating(function ($model) {
+            // ... code here
+        });
+
+        self::updated(function ($model) {
+            // ... code here
+        });
+
+        self::deleting(function ($model) {
+            // ... code here
+        });
+
+        self::deleted(function ($model) {
+            // ... code here
+        });
+    }
 }
