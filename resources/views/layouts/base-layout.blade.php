26ad6f9897f2482086e002a694096acea7072ea6
febryntara <paglutgede@gmail.com>
2022-12-28 14:32:31 +0800

    fix error yang dibuat denny

--- resources/views/layouts/base-layout.blade.php
+++ resources/views/layouts/base-layout.blade.php
@@ -6,6 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     @yield('base_head')
+    <link rel="stylesheet" href="{{ asset('dist/css/_app.css') }}">
     <link rel="stylesheet" href="{{ asset('dist/css/app.css') }}">
 </head>
 
