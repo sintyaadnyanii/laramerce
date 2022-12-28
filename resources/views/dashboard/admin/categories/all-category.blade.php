3de200a9bcbbddc73df198bb16014ae2ac8ae2f3
Kaari <110882642+larxene11@users.noreply.github.com>
2022-12-28 12:22:17 +0800

    form view

--- resources/views/dashboard/admin/categories/all-category.blade.php
+++ resources/views/dashboard/admin/categories/all-category.blade.php
@@ -1,7 +1,7 @@
 @extends('layouts.dashboard-layout')
 @section('dashboard-content')
     <h2 class="intro-y text-lg font-medium mt-10">
-        All Category
+        Data Category List
     </h2>
     <div class="grid grid-cols-12 gap-6 mt-5">
         <div class="intro-y col-span-12 flex flex-wrap sm:flex-nowrap items-center mt-2">
