d21aaf9cdbf58d8757652022899efe33e348ec74
Strelok-91 <alexanderivanov416@gmail.com>
2022-12-29 09:30:47 +0800

    hjghjgfhjf

--- resources/views/dashboard/user/orders/all-order.blade.php
+++ resources/views/dashboard/user/orders/all-order.blade.php
@@ -1,11 +1,11 @@
 @extends('layouts.dashboard-layout')
 @section('dashboard-content')
-<h2 class="intro-y text-lg font-medium mt-10">
-        All Cart
+    <h2 class="intro-y text-lg font-medium mt-10">
+        All Order
     </h2>
     <div class="grid grid-cols-12 gap-6 mt-5">
         <div class="intro-y col-span-12 flex flex-wrap sm:flex-nowrap items-center mt-2">
-            <a href="#" class="btn btn-primary shadow-md mr-2">Add New Cart</a>
+            <a href="#" class="btn btn-primary shadow-md mr-2">Add New Order</a>
             <div class="dropdown">
                 <button class="dropdown-toggle btn px-2 box" aria-expanded="false" data-tw-toggle="dropdown">
                     <span class="w-5 h-5 flex items-center justify-center"> <i class="w-4 h-4" data-lucide="plus"></i> </span>
@@ -33,14 +33,18 @@
             </div>
         </div>
         <!-- BEGIN: Data List -->
-        <div class="intro-y col-span-12 overflow-auto lg:overflow-visible">
+        <div class="intro-y col-span-12 overflow-x-auto">
             <table class="table table-report -mt-2">
                 <thead>
                     <tr>
                         <th class="whitespace-nowrap">No.</th>
-                        <th class="whitespace-nowrap">USER</th>
-                        <th class="text-center whitespace-nowrap">PRODUCT</th>
-                        <th class="text-center whitespace-nowrap">AMOUNT</th>
+                        <th class="whitespace-nowrap">NAME</th>
+                        <th class="whitespace-nowrap">PHONE</th>
+                        <th class="text-center whitespace-nowrap">EMAIL</th>
+                        <th class="whitespace-nowrap">GROSS AMOUNT</th>
+                        <th class="whitespace-nowrap">TRANSACTION ID</th>
+                        <th class="whitespace-nowrap">PAYMENT TOKEN</th>
+                        <th class="whitespace-nowrap">PAYMENT TYPE</th>
                         <th class="text-center whitespace-nowrap">ACTIONS</th>
                     </tr>
                 </thead>
@@ -52,10 +56,14 @@
                             </div>
                         </td>
                         <td>
-                            <a href="" class="font-medium whitespace-nowrap">John Doe</a> 
+                            <a href="" class="font-medium whitespace-nowrap">Lipsum</a> 
                         </td>
                         <td class="text-center">Lorem Ipsum</td>
-                        <td class="text-center">10</td>
+                        <td class="text-center">Lorem Ipsum@mail.com</td>
+                        <td class="text-center">Lorem Ipsum</td>
+                        <td class="text-center">Lorem Ipsum</td>
+                        <td class="text-center">Lorem Ipsum</td>
+                        <td class="text-center">Lorem Ipsum</td>
                         <td class="table-report__action w-56">
                             <div class="flex justify-center items-center">
                                 <a class="flex items-center mr-3" href="javascript:;"> <i data-lucide="check-square" class="w-4 h-4 mr-1"></i> Edit </a>
