59860d9fda25ea30ccdb905d94e90b174f831b05
Strelok-91 <alexanderivanov416@gmail.com>
2022-12-31 11:01:14 +0800

    ORDERS

---
+++ resources/views/dashboard/admin/orders/order-all.blade.php
@@ -0,0 +1,138 @@
+@extends('layouts.dashboard-layout')
+@section('dashboard-content')
+    <h2 class="intro-y text-lg font-medium mt-10">
+        All Order
+    </h2>
+    <div class="grid grid-cols-12 gap-6 mt-5">
+        <div class="intro-y col-span-12 flex flex-wrap sm:flex-nowrap items-center mt-2">
+            <a href="{{ route('manage_order.create') }}" class="btn btn-primary shadow-md mr-2">Add New Order</a>
+            <div class="dropdown">
+                <button class="dropdown-toggle btn px-2 box" aria-expanded="false" data-tw-toggle="dropdown">
+                    <span class="w-5 h-5 flex items-center justify-center"> <i class="w-4 h-4" data-lucide="plus"></i> </span>
+                </button>
+                <div class="dropdown-menu w-40">
+                    <ul class="dropdown-content">
+                        <li>
+                            <a href="" class="dropdown-item"> <i data-lucide="printer" class="w-4 h-4 mr-2"></i> Print </a>
+                        </li>
+                        <li>
+                            <a href="" class="dropdown-item"> <i data-lucide="file-text" class="w-4 h-4 mr-2"></i> Export to Excel </a>
+                        </li>
+                        <li>
+                            <a href="" class="dropdown-item"> <i data-lucide="file-text" class="w-4 h-4 mr-2"></i> Export to PDF </a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+            <div class="hidden md:block mx-auto text-slate-500">Showing 1 to 10 of 150 entries</div>
+            <div class="w-full sm:w-auto mt-3 sm:mt-0 sm:ml-auto md:ml-0">
+                <div class="w-56 relative text-slate-500">
+                    <input type="text" class="form-control w-56 box pr-10" placeholder="Search...">
+                    <i class="w-4 h-4 absolute my-auto inset-y-0 mr-3 right-0" data-lucide="search"></i> 
+                </div>
+            </div>
+        </div>
+        <!-- BEGIN: Data List -->
+        <div class="intro-y col-span-12 overflow-auto lg:overflow-visible">
+            <table class="table table-report -mt-2">
+                <thead>
+                    <tr>
+                        <th class="text-center whitespace-nowrap">No.</th>
+                        <th class="text-center whitespace-nowrap">NAME</th>
+                        <th class="text-center whitespace-nowrap">PHONE</th>
+                        <th class="text-center whitespace-nowrap">GROSS AMOUNT</th>
+                        <th class="text-center whitespace-nowrap">PAYMENT</th>
+                        <th class="text-center whitespace-nowrap">ACTIONS</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    @forelse ($order as $index=>$item)
+                     <tr class="intro-x">
+                        <td class="text-center w-40"> {{ $loop->iteration }} </td>
+                        <td class="text-center">{{ $item->name }}</td>
+                        <td class="text-center">{{ $item->phone }}</td>
+                        <td class="text-center">{{ $item->gross_amount }}</td>
+                        <td class="text-center">{{ $item->payment_type }}</td>
+                        <td class="table-report__action w-56">
+                            <div class="flex justify-center items-center">
+                                <a class="flex items-center mr-3" href="{{ route('manage_category.detail',['category'=>$item]) }}"> <i data-lucide="eye" class="w-4 h-4 mr-1"></i> Detail </a>
+                                <a class="flex items-center mr-3" href="{{ route('manage_category.update',['category'=>$item]) }}"> <i data-lucide="check-square" class="w-4 h-4 mr-1"></i> Edit </a>
+                                <a class="flex items-center text-danger" href="javascript:;" data-tw-toggle="modal" data-tw-target="#delete-confirmation-modal" onclick="deleteModalHandler({{$index}})"> <i data-lucide="trash-2" class="w-4 h-4 mr-1"></i> Delete </a>
+                                <input type="hidden" id="delete_route_{{$index}}" value="{{ route('manage_category.delete',['category'=>$item]) }}">
+                            </div>
+                        </td>
+                    </tr>   
+                    @empty
+                    <tr>
+                        <td class="text-center text-muted" colspan="5">No Data</td>
+                    </tr>
+                    @endforelse
+                    
+                    
+                </tbody>
+            </table>
+        </div>
+        <!-- END: Data List -->
+        <!-- BEGIN: Pagination -->
+        <div class="intro-y col-span-12 flex flex-wrap sm:flex-row sm:flex-nowrap items-center">
+            <nav class="w-full sm:w-auto sm:mr-auto">
+                <ul class="pagination">
+                    <li class="page-item">
+                        <a class="page-link" href="#"> <i class="w-4 h-4" data-lucide="chevrons-left"></i> </a>
+                    </li>
+                    <li class="page-item">
+                        <a class="page-link" href="#"> <i class="w-4 h-4" data-lucide="chevron-left"></i> </a>
+                    </li>
+                    <li class="page-item"> <a class="page-link" href="#">...</a> </li>
+                    <li class="page-item"> <a class="page-link" href="#">1</a> </li>
+                    <li class="page-item active"> <a class="page-link" href="#">2</a> </li>
+                    <li class="page-item"> <a class="page-link" href="#">3</a> </li>
+                    <li class="page-item"> <a class="page-link" href="#">...</a> </li>
+                    <li class="page-item">
+                        <a class="page-link" href="#"> <i class="w-4 h-4" data-lucide="chevron-right"></i> </a>
+                    </li>
+                    <li class="page-item">
+                        <a class="page-link" href="#"> <i class="w-4 h-4" data-lucide="chevrons-right"></i> </a>
+                    </li>
+                </ul>
+            </nav>
+            <select class="w-20 form-select box mt-3 sm:mt-0">
+                <option>10</option>
+                <option>25</option>
+                <option>35</option>
+                <option>50</option>
+            </select>
+        </div>
+        <!-- END: Pagination -->
+    </div>
+    <!-- BEGIN: Delete Confirmation Modal -->
+    <div id="delete-confirmation-modal" class="modal" tabindex="-1" aria-hidden="true">
+        <div class="modal-dialog">
+            <div class="modal-content">
+                <div class="modal-body p-0">
+                    <div class="p-5 text-center">
+                        <i data-lucide="x-circle" class="w-16 h-16 text-danger mx-auto mt-3"></i> 
+                        <div class="text-3xl mt-5">Are you sure?</div>
+                        <div class="text-slate-500 mt-2">
+                            Do you really want to delete these records? 
+                            <br>
+                            This process cannot be undone.
+                        </div>
+                    </div>
+                    <div class="px-5 pb-8 flex justify-center items-center">
+                        <button type="button" data-tw-dismiss="modal" class="btn btn-outline-secondary w-24 mr-1">Cancel</button>
+                        <form id="deleteItem" method="post">
+                            @csrf
+                            @method('delete')
+                            <input type="hidden" value="" id="delete_route_input">
+                            <button type="submit" class="btn btn-danger w-24">Delete</button>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+@endsection
+@section('script')
+   <script src="{{ asset('dist/js/view/manage-category/category.js') }}"></script> 
+@endsection
---
+++ resources/views/dashboard/admin/orders/order-edit.blade.php
@@ -0,0 +1,93 @@
+@extends('layouts.dashboard-layout')
+@section('dashboard-content')
+<!-- BEGIN: Content -->
+<div class="content">
+    <!-- END: Top Bar -->
+    <div class="intro-y flex items-center mt-8">
+        <h2 class="text-lg font-medium mr-auto">
+            Edit {{$order->name}}
+        </h2>
+    </div>
+    <div class="grid grid-cols-12 gap-6 mt-5">
+        <div class="intro-y col-span-12 lg:col-span-12">
+            <!-- BEGIN: Form Layout -->
+            <form action="{{ route('manage_order.patch',['order'=>$order]) }}" method="POST">
+                @csrf
+                @method('patch')
+            <div class="intro-y box p-5">
+                <div>
+                    <label for="name" class="form-label">User ID</label>
+                    @error('name')
+                        <small class="text-xs text-red-500 ml-1">{{'*'.$message }}</small>
+                    @enderror
+                    <input type="number" name="user_id" id="order_user_id" class="form-control w-full" placeholder="Input User ID" value="{{ $order->user_id ?? old('user_id') }}">
+                </div>
+
+                <div>
+                    <label for="name" class="form-label">Order Name</label>
+                    @error('order_name')
+                        <small class="text-xs text-red-500 ml-1">{{'*'.$message }}</small>
+                    @enderror
+                    <input type="text" name="name" id="order_name" class="form-control w-full" placeholder="Input Order Name" value="{{ $order->name ?? old('name') }}">
+                </div>
+
+                <div>
+                    <label for="name" class="form-label">Phone</label>
+                    @error('order_phone')
+                        <small class="text-xs text-red-500 ml-1">{{'*'.$message }}</small>
+                    @enderror
+                    <input type="text" name="phone" id="order_phone" class="form-control w-full" placeholder="Input Phone" value="{{ $order->phone ?? old('phone') }}">
+                </div>
+
+                <div>
+                    <label for="name" class="form-label">Email</label>
+                    @error('order_email')
+                        <small class="text-xs text-red-500 ml-1">{{'*'.$message }}</small>
+                    @enderror
+                    <input type="email" name="email" id="order_email" class="form-control w-full" placeholder="Input Email" value="{{ $order->email ?? old('email') }}">
+                </div>
+
+                <div>
+                    <label for="name" class="form-label">Gross Amount</label>
+                    @error('gross_amount')
+                        <small class="text-xs text-red-500 ml-1">{{'*'.$message }}</small>
+                    @enderror
+                    <input type="number" name="gross_amount" id="order_gross_amount" class="form-control w-full" placeholder="Input Gross Amount" value="{{ $order->gross_amount ?? old('gross_amount') }}">
+                </div>
+
+                <div>
+                    <label for="name" class="form-label">Transaction ID</label>
+                    @error('transaction_id')
+                        <small class="text-xs text-red-500 ml-1">{{'*'.$message }}</small>
+                    @enderror
+                    <input type="number" name="transaction_id" id="order_transaction_id" class="form-control w-full" placeholder="Input Transaction ID" value="{{ $order->transaction_id ?? old('transaction_id') }}">
+                </div>
+
+                <div>
+                    <label for="name" class="form-label">Payment Token</label>
+                    @error('payment_token')
+                        <small class="text-xs text-red-500 ml-1">{{'*'.$message }}</small>
+                    @enderror
+                    <input type="text" name="payment_token" id="order_payment_token" class="form-control w-full" placeholder="Input Payment Token" value="{{ $order->payment_token ?? old('payment_token') }}">
+                </div>
+
+                <div>
+                    <label for="name" class="form-label">Payment Type</label>
+                    @error('payment_type')
+                        <small class="text-xs text-red-500 ml-1">{{'*'.$message }}</small>
+                    @enderror
+                    <input type="text" name="payment_type" id="order_payment_type" class="form-control w-full" placeholder="Input Payment Type" value="{{ $order->payment_type ?? old('payment_type') }}">
+                </div>
+                
+                <div class="text-right mt-5">
+                    <a class="btn btn-outline-secondary w-24 mr-1" href="{{ route('manage_category.all') }}">Cancel</a>
+                    <button type="submit" class="btn btn-primary shadow-md w-24 mr-1">Save</button>
+                </div>
+            </div>
+        </form>
+        <!-- END: Form Layout -->
+        </div>
+    </div>
+</div>
+<!-- END: Content -->
+@endsection
