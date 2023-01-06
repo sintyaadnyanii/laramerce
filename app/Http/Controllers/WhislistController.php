4e32d991edaaa16742ea74f2c288020f6342a730
febryntara <paglutgede@gmail.com>
2023-01-06 15:02:50 +0800

    crud ajax wishlist and cart

--- app/Http/Controllers/WhislistController.php
+++ app/Http/Controllers/WhislistController.php
@@ -2,9 +2,54 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Whislist;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
 
 class WhislistController extends Controller
 {
-    //
+    public function wish(Request $request)
+    {
+        $validator = Validator::make($request->all(), [
+            'product_code' => 'required|numeric',
+            'user_id' => 'required|numeric',
+        ]);
+
+        if ($validator->fails()) {
+            return response()->json([
+                'status' => 400,
+                'message' => 'Validation Failed!'
+            ]);
+        }
+        $validated = $validator->validate();
+        $product_on_wish = Whislist::where('user_id', $validated['user_id'])->orWhere('product_code', $validated['product_code'])->get();
+        if ($product_on_wish->count()) {
+            if ($product_on_wish->first()->delete()) {
+                return response()->json([
+                    'status' => 200,
+                    'message' => 'Wish Product Deleted!'
+                ]);
+            } else {
+                return response()->json([
+                    'status' => 201,
+                    'message' => 'Something Went Wrong!'
+                ]);
+            }
+        } else {
+            if (Whislist::create([
+                'product_code' => $validated['product_code'],
+                'user_id' => $validated['user_id'],
+            ])) {
+                return response()->json([
+                    'status' => 200,
+                    'message' => 'Product Added To WishList!'
+                ]);
+            } else {
+                return response()->json([
+                    'status' => 201,
+                    'message' => 'Something Went Wrong!'
+                ]);
+            }
+        }
+    }
 }
--- app/Models/Whislist.php
+++ app/Models/Whislist.php
@@ -8,4 +8,10 @@ use Illuminate\Database\Eloquent\Model;
 class Whislist extends Model
 {
     use HasFactory;
+    protected $fillable = ['product_code', 'user_id'];
+
+    public function product()
+    {
+        return $this->belongsTo(Product::class, 'product_code');
+    }
 }
