94eb09ebf5bc41416e7e4f05aaaac752826a9d20
dennykd <kadekdenny61@gmail.com>
2023-01-13 09:34:44 +0800

    partial front page init

--- resources/views/layouts/base-layout.blade.php
+++ resources/views/layouts/base-layout.blade.php
@@ -2,29 +2,16 @@
 <html lang="en">
 
 <head>
+    <title>{{ $title }}</title>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     @yield('base_head')
-    <link rel="stylesheet" href="{{ asset('dist/css/_app.css') }}">
-    <link rel="stylesheet" href="{{ asset('dist/css/app.css') }}">
 </head>
 
 <body>
     @yield('base_body')
-    <script src="https://code.jquery.com/jquery-3.6.3.min.js"
-        integrity="sha256-pvPw+upLPUjgMXY0G+8O0xUf+/Im1MZjXxxgOcBQBXU=" crossorigin="anonymous"></script>
-    <script src="{{ asset('dist/js/app.js') }}"></script>
-    <script src="https://unpkg.com/flowbite@1.5.5/dist/flowbite.js"></script>
-    <script src="https://cdn.tiny.cloud/1/1cwcv7fb2ka7wchr47abco6ychaqqw5fpjdp5ssh1ea863qp/tinymce/6/tinymce.min.js"
-        referrerpolicy="origin"></script>
-    <script>
-        tinymce.init({
-            selector: 'textarea',
-            plugins: 'anchor autolink charmap codesample emoticons image link lists media searchreplace table visualblocks wordcount',
-            toolbar: 'undo redo | blocks fontfamily fontsize | bold italic underline strikethrough | link image media table | align lineheight | numlist bullist indent outdent | emoticons charmap | removeformat',
-        });
-    </script>
+
     @yield('base_script')
 </body>
 
