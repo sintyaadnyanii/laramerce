2b400ac4d549a34c0d2a914665d65e8dc9ccf19a
Strelok-91 <alexanderivanov416@gmail.com>
2023-01-18 10:01:14 +0800

    overflow

--- resources/views/dashboard/admin/products/product-all.blade.php
+++ resources/views/dashboard/admin/products/product-all.blade.php
@@ -33,7 +33,7 @@
         </div>
     </div>
     <!-- BEGIN: Data List -->
-    <div class="intro-y col-span-12 overflow-auto lg:overflow-visible">
+    <div class="intro-y col-span-12 overflow-auto">
         <table class="table table-report -mt-2">
             <thead>
                 <tr>
