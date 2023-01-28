3969e99a0f7970fa8465f19e7cf0047494c06934
dennykd <kadekdenny61@gmail.com>
2023-01-28 13:48:47 +0800

    changed favicon

--- resources/views/layouts/front-layout.blade.php
+++ resources/views/layouts/front-layout.blade.php
@@ -11,7 +11,7 @@
     <meta name="robots" content="index, follow" />
     <meta name="viewport" content="width=device-width,initial-scale=1">
 
-    <link rel="shortcut icon" type="image/png" href="{{ asset('image/catalog/favicon.jpg') }}" />
+    <link rel="shortcut icon" type="image/png" href="{{ asset('image/catalog/urban.png') }}" />
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/swiper/swiper-bundle.min.css" />
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet"
         integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
@@ -19,7 +19,7 @@
         integrity="sha384-w76AqPfDkMBDXo30jS1Sgez6pr3x5MlQ1ZAGC+nuZB+EYdgRZgiwxhTBTkF7CXvN" crossorigin="anonymous">
     </script>
     <!-- Libs CSS
-                                                                                                                                        ============================================ -->
+                                                                                                                                                ============================================ -->
     <link rel="stylesheet" href="{{ asset('/css/bootstrap/css/bootstrap.min.css') }}">
     <link href="{{ asset('/css/font-awesome/css/font-awesome.min.css') }}" rel="stylesheet">
     <link href="{{ asset('/js/datetimepicker/bootstrap-datetimepicker.min.css') }}" rel="stylesheet">
@@ -29,7 +29,7 @@
     <link href="{{ asset('/js/minicolors/miniColors.css') }}" rel="stylesheet">
 
     <!-- Theme CSS
-                                                                                                                                        ============================================ -->
+                                                                                                                                                ============================================ -->
     <link href="{{ asset('/css/themecss/so_searchpro.css') }}" rel="stylesheet">
     <link href="{{ asset('/css/themecss/so_megamenu.css') }}" rel="stylesheet">
     <link href="{{ asset('/css/themecss/so-categories.css') }}" rel="stylesheet">
@@ -43,7 +43,7 @@
     <link href="{{ asset('/css/responsive.css') }}" rel="stylesheet">
 
     <!-- Google web fonts
-                                                                                                                                ============================================ -->
+                                                                                                                                        ============================================ -->
     <link href="https://fonts.googleapis.com/css?family=Open+Sans:400,600,700" rel='stylesheet' type='text/css'>
     <link href="https://fonts.googleapis.com/css?family=Raleway:400,500,600,700" rel='stylesheet' type='text/css'>
     <style>
